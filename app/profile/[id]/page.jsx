ba1ebbc97f957320e9acf8b47b4c871a83b54a78
Blink1702 <bernardo.oyervides17@gmail.com>
2025-02-24 21:52:18 -0600

    fixed profile pages

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import { notFound } from "next/navigation"
+
+export const dynamicParams = true
+
+/*export async function generateStaticParams() {
+  const res = await fetch(`http://localhost:8085/users/profile`)
+  const profiles = await res.json()
+
+  return profiles.map((profile) => ({
+      id: profile.id
+  }))
+}*/
+
+async function getProfile(id) {
+  const res = await fetch(`http://localhost:8085/users/profile/${id}`,{
+    next: {
+      revalidate : 60
+    }
+  })
+
+  if (!res.ok) {
+      notFound()
+  }
+  
+  return res.json()
+}
 
 
-export default function ProfilePage() {
+export default async function ProfileDetails({params}) {
+  const profile = await getProfile(params.id)
   return (
-    <main></main>
+    <main>
+        <h2>Profile</h2>
+        <h4>LU ID: {profile.luid}</h4>
+        <h4>Email: {profile.email}</h4>
+    </main>
+
   )
 }
